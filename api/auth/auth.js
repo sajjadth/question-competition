0556dcb9d93559f128a6ccea3cbfa2e4eda404c2
sajjad <st.655.st@gmail.com>
2021-07-17 10:57:28 +0430

    fixed jwt errors

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -3,10 +3,15 @@ const jwt = require("jsonwebtoken");
 module.exports = function(req, res, next) {
   const token = req.header("auth-token");
   if (!token) res.send("access denied!");
-  
+
   jwt.verify(token, process.env.TOKEN_SECRET, (err, data) => {
     if (err) {
-      console.log(err);
+      if (err.message === "jwt expired" || err.message === "invalid signature") {
+        req.error = { success: false, message: err.message };
+        next();
+      } else {
+        console.log("twt error");
+      }
     } else {
       req.user = data;
       next();
